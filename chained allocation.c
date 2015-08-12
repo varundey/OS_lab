aec1aea6d8760ee2d540c78a249dd4306140027e
Varun Dey <varun.silversurfer@gmail.com>
2015-08-12 10:29:55 +0530

    chained allocation method for file allocation

---
+++ chained allocation.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+	int mem,i,req,alloc_no,n;
+	char s[50];
+	printf("Enter file name\n");
+	scanf("%s",&s);
+	
+	printf("Enter number of blocks in the main memory\n");
+	scanf("%d",&mem);
+	int bloc_no[mem];
+	for (i=1;i<=mem;i++){
+		bloc_no[i]=0;
+	}
+	
+	printf("Enter number of already allocated blocks\n");
+	scanf("%d",&alloc_no);
+	printf("Enter %d blocks which are allocated\n",alloc_no);
+	for (i=1;i<=alloc_no;i++){
+		scanf("%d",&n);
+		bloc_no[n]=1;
+	}
+	
+	printf("Enter number of required block\n");
+	scanf("%d",&req);
+	
+	if(req<=mem-alloc_no){
+		printf("Allocated blocks are\n");
+		for (i=1;i<=mem;i++){
+			if (bloc_no[i]==1){
+				continue;
+			}
+			else{
+				printf("%d\t",i);
+			}
+		}
+	}
+	else    printf("Not enough space!!");
+	
+	return 0;
+}
